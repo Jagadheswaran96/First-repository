11b4fc7280864bca0acbf3ff55b6e0b2e12898e2
Jagadheswaran <jagadhesjack88@gmail.com>
2025-01-22 22:33:17 +0530

    Java and API concepts

---
+++ Hands-On/src/practice/ReadExcelFileData.java
@@ -0,0 +1,41 @@
+package practice;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ReadExcelFileData {
+    public static void main(String[] args) {
+        String filePath = "path/to/your/excel/file.xlsx";
+
+        try (FileInputStream fis = new FileInputStream(new File(filePath));
+             Workbook workbook = new XSSFWorkbook(fis)) {
+
+            Sheet sheet = workbook.getSheetAt(0);  // Get the first sheet
+            for (Row row : sheet) {
+                for (Cell cell : row) {
+                    switch (cell.getCellType()) {
+                        case STRING:
+                            System.out.print(cell.getStringCellValue() + "\t");
+                            break;
+                        case NUMERIC:
+                            System.out.print(cell.getNumericCellValue() + "\t");
+                            break;
+                        case BOOLEAN:
+                            System.out.print(cell.getBooleanCellValue() + "\t");
+                            break;
+                        default:
+                            System.out.print("Unknown Value\t");
+                    }
+                }
+                System.out.println();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Hands-On/src/practice/SerializableInterface.java
@@ -0,0 +1,19 @@
+package practice;
+
+import java.io.*;
+
+public class SerializableInterface {
+
+	public static void main(String[] args) {
+
+		/* Serialization in Java is the process of converting an object into a byte stream to save it to a file, send it over a network, or store it in a database. The object can later be deserialized to recreate the original object.
+
+		Steps to Handle Serialization:
+		
+		Implement the Serializable interface in the class.
+		Use ObjectOutputStream to write the object to a file.
+		Use ObjectInputStream to read the object from a file. */
+		
+	}
+
+}
